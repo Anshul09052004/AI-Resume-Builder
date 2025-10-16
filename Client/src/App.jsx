a2a61b59d0c063e47f0f408420d0f7db9f97082a
Anshul prajapat <anshulprajapat982@gmail.com>
2025-10-16 12:49:07 +0530

    initialization setup

---
+++ Client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl text-violet-900'>AI Powered Resume Builder</h1>
+
+    </>
+  )
+}
+
+export default App
