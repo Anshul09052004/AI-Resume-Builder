8ac7b5235d015678b255092710d17852446a7293
Anshul prajapat <anshulprajapat982@gmail.com>
2025-10-28 12:14:20 +0530

    deploy changes

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ app.set('trust proxy', 1);
 app.use(express.json());
 app.use(cors({
   origin: [
-    "http://localhost:5173"
+    "https://ai-resume-builder-frontend-73kc.onrender.com"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
